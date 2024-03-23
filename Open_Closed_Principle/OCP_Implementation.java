da68960e39bdc28fd288ee6c664353f637fa32bb
Pratham Mishra <mishrapratham119@gmail.com>
2024-03-23 22:41:22 +0530

    Updating the structure and removing out directory

--- Open_Closed_Principle/OCP_Implementation.java
+++ Open_Closed_Principle/OCP_Implementation.java
@@ -38,5 +38,6 @@ class main{
 }
 
 /*
-In this example, the  OCP_Implementation  class is open for extension because you can easily add new shapes by creating new classes that implement the  Shape  interface without modifying the  Drawing  class. This way, the  ocp_implementation class is closed for modification but open for extension, following the Open-Closed Principle.
+In this example, the  OCP_Implementation  class is open for extension because you can easily add new shapes by creating
+new classes that implement the  Shape  interface without modifying the  Drawing  class. This way, the  ocp_implementation class is closed for modification but open for extension, following the Open-Closed Principle.
  */
