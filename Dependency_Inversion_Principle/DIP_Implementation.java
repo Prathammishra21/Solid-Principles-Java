207aa240f1c9d6c43c6f425659af63bef1466a26
Pratham Mishra <mishrapratham119@gmail.com>
2024-03-24 11:55:03 +0530

    Added isp and dip implementation

--- Dependency_Inversion_Principle/DIP_Implementation.java
+++ Dependency_Inversion_Principle/DIP_Implementation.java
@@ -1,4 +1,45 @@
 package Dependency_Inversion_Principle;
 
+interface Printer{
+    void print(String message);
+}
+
+class ConsolePrinter implements Printer{
+    @Override
+    public void print(String message){
+        System.out.println("Printing to console: " +message);
+    }
+}
+
+class FilePrinter implements Printer{
+    @Override
+    public void print(String message){
+        System.out.println("Printing to file: "+message);
+    }
+}
+
+// High-level module
+class Logger {
+    private final Printer printer;
+
+    // Constructor injection
+    Logger(Printer printer) {
+        this.printer = printer;
+    }
+
+    void log(String message) {
+        printer.print(message);
+    }
+}
+
 public class DIP_Implementation {
+    public static void main(String[] args) {
+        // Creating instances of the Logger class with different types of printers
+        Logger consoleLogger = new Logger(new ConsolePrinter());
+        Logger fileLogger = new Logger(new FilePrinter());
+
+        // Logging messages
+        consoleLogger.log("Message logged to console");
+        fileLogger.log("Message logged to file");
+    }
 }
--- Interface_Segregation_Principle/ISP_Implementation.java
+++ Interface_Segregation_Principle/ISP_Implementation.java
@@ -1,4 +1,54 @@
 package Interface_Segregation_Principle;
 
+interface Shape {
+    int area();
+}
+
+//Interface for shapes with dimensions
+interface DimensionalShape extends Shape {
+    int getWidth();
+
+    int getHeight();
+}
+
+//Rectangle class implementing DimensionalShape
+class Rectangle implements DimensionalShape {
+    int width, height;
+
+    @Override
+    public int area() {
+        return width * height;
+    }
+
+    @Override
+    public int getWidth() {
+        return width;
+    }
+
+    @Override
+    public int getHeight() {
+        return height;
+    }
+}
+
+class Circle implements Shape {
+    int radius;
+
+    @Override
+    public int area() {
+        return (int) (Math.PI * radius * radius);
+    }
+}
+
 public class ISP_Implementation {
+    public static void main(String[] args) {
+        Rectangle rectangle = new Rectangle();
+        rectangle.height = 10;
+        rectangle.width = 3;
+        System.out.println("Area of rectangle: " +rectangle.area());
+
+        Circle circle = new Circle();
+        circle.radius = 6;
+        System.out.println("Area of circle: " +circle.area());
+    }
 }
